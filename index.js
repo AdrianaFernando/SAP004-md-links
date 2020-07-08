60bee5918de346498687c3ac4d6fecbfd29b8b94
Adriana Fernando <adriana.drih@gmail.com>
2020-07-07 22:28:16 -0300

    iniciando a função para ler o md

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
-};
+}; */
+
+const fs = require('fs');
+const path = require('path');
+
+const file = path.join(__dirname, 'README.md')
+
+/* const data = fs.readFileSync('README.md');
+console.log(data.toString()); */
+
+fs.readFile(file,'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
