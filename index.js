c22741ddad2b97bf58a12c3b70a53b5c18d5bb24
Adriana Fernando <adriana.drih@gmail.com>
2020-07-13 16:24:39 -0300

    atualizando README

--- index.js
+++ index.js
@@ -5,55 +5,18 @@
 const fs = require('fs');
 const path = require('path');
 
-/* const mdPath = path.join(__dirname, 'files')
+  const file = path.join(__dirname, 'testeparaleitura.md')
 
-const mdRead = fs.readdir(mdPath, function(err, files) {
-  if (err) {
-    console.log("Nenhum arquivo .md encontrado.")
-  } else {
-    const mdFile = files.filter(function (file){
-      return path.extname(file) == '.md'
-    })
-    mdFile.forEach(function(file) {
-      console.log(file)
-    })
-  }
-}) */
-
-/* const file = path.join(__dirname, 'README.md')
-
-const mdLinks = (file) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file,'utf8', (err, data) => {
-      if (err) {
-        reject ('Nao encontrado');
-  }else{
-    const regEx = /\[([^\[\]]+)\]\((http[s]?:[^\)]*)\)/gm
-    const matchRegex = data.match(regEx);
-    const arrayRegex = [];
-    matchRegex.forEach((element) =>{
-      const infos = {
-        text: `${element.match(/\[([^\[\]]+)\]/)}`,
-        href: `${element.match(/\((http[s]?:[^\)]*)\)/)}`,
-        file: file
-      };
-      arrayRegex.push(infos)
-    })
-    return resolve(arrayRegex)
-  }
-});
-  });
-  }
-   */
-  const file = path.join(__dirname, 'README.md')
+  const mdLinks = (file) => {
+    return new Promise((resolve, reject) => {
    fs.readFile(file,'utf8', (err, data) => {
       if (err) {
         reject ('Nao encontrado');
-  }else{
-    const regEx = /\[([^\[\]]+)\]\((http[s]?:[^\)]*)\)/gm
-    const matchRegex = data.match(regEx);
-    const arrayRegex = [];
-    matchRegex.forEach((element) =>{
+      }else{
+      const regEx = /\[([^\[\]]+)\]\((http[s]?:[^\)]*)\)/gm
+      const matchRegex = data.match(regEx);
+      const arrayRegex = [];
+      matchRegex.forEach((element) =>{
       const infos = {
         text: `${element.match(/\[([^\[\]]+)\]/)}`,
         href: `${element.match(/\((http[s]?:[^\)]*)\)/)}`,
@@ -61,6 +24,12 @@ const mdLinks = (file) => {
       };
       arrayRegex.push(infos)
     })
-    console.log(arrayRegex)
-  }
-});
+    return resolve (arrayRegex)
+    //console.log(arrayRegex)
+    }
+    })
+    })
+  };
+  //module.exports = mdLinks;
+
+
